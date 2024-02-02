b3da4d7d2923bae0c0e74579a2799e00429fbbc6
Tevin Campbell <75921907+Troyzhenny@users.noreply.github.com>
2024-02-02 00:47:23 -0500

    tweaked layout for project page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="container">
-      <div v-for="(card, index) in cards" :key="index" class="card">
-        <img :src="card.image" alt="Card Image" class="card-image">
-        <p class="description">{{ card.description }}</p>
+  <div class="container">
+      <div class="heading--cont">
+        <h2>SELECTED PROJECTS</h2>
+      </div>
+      <div class="row">
+        <div v-for="(card, index) in cards" :key="index" class="card">
+          <img :src="card.image" alt="Card Image" class="card-image">
+          <h2 class="title">{{ card.title }} <i class="fa-solid fa-arrow-right"></i></h2>
+          <p class="description">{{ card.description }}</p>
+        </div>
       </div>
     </div>
   </template>
@@ -13,10 +19,12 @@
   export default {
     setup() {
       const cards = ref([
-        { image: 'src/assets/images/z.png', description: 'Description 1' },
-        { image: 'src/assets/images/z.png', description: 'Description 2' },
-        { image: 'src/assets/images/z.png', description: 'Description 3' },
-        { image: 'src/assets/images/z.png', description: 'Description 4' }
+        { title: 'tevintc.xyz', image: 'src/assets/images/z.png', description: 'lorem Ipsum Is my bestest friend in the entire world.' },
+        { title: 'playstationclone.xyz', image: 'src/assets/images/z.png', description: 'lorem Ipsum Is my bestest friend in the entire world.' },
+        { title: 'kyromerch.shop', image: 'src/assets/images/z.png', description: 'lorem Ipsum Is my bestest friend in the entire world.' },
+        { title: 'dominospos.app', image: 'src/assets/images/z.png', description: 'lorem Ipsum Is my bestest friend in the entire world.' },
+        { title: 'expensetracker.app', image: 'src/assets/images/z.png', description: 'lorem Ipsum Is my bestest friend in the entire world.' },
+        { title: 'pbrp.site', image: 'src/assets/images/z.png', description: 'lorem Ipsum Is my bestest friend in the entire world.' }
       ]);
   
       return { cards };
@@ -28,22 +36,56 @@
 @import "../assets/scss/variables";
   .container {
     display: flex;
-    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
     justify-content: center;
+    width: 100%;
+    min-height: 100vh;
 
     color: $light;
     background-color: $dark;
+
+    .heading--cont {
+      display: flex;
+      width: 100%;
+    
+      h2 {
+        font-size: 5em;
+        margin: 60px;
+      }
+    }
+
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      width: 100%;
+      height: auto;
+      margin-top: 50px;
+
+      i {
+        font-size: .70em;
+        transform: rotateZ(-45deg);
+      }
+    }
   }
   .card {
     border: 1px solid #ccc;
-    padding: 10px;
-    margin: 10px;
+    border-radius: 5px;
+    width: 500px;
+    height: 500px;
+    padding: 90px;
+    margin: 50px;
     text-align: center;
   
     .card-image {
       width: 100%;
-      max-height: 200px; // Adjust as needed
+      max-height: 200px;
       object-fit: cover;
+    }
+
+    .title {
+     font-weight: 600; 
     }
   
     .description {
