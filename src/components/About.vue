4486dfc2788d321d07399e75aa85efd52833a768
Tevin Campbell <75921907+Troyzhenny@users.noreply.github.com>
2024-02-10 15:31:35 -0500

    updated bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,11 +12,6 @@
                 I am an enthusiastic web developer and graphic designer located in North Carolina.
                 With a passion for my craft, I excel in crafting sleek, minimalist, and responsive
                 web solutions that not only meet but exceed client expectations.
-
-                I am eager to embark on a journey where I can not only
-                showcase my skills but also continually enhance them.
-                I am committed to evolving into an even more proficient and versatile version
-                of myself.
                 <br><br>
                 When I'm not coding or designing, I enjoy spending time with my daughter, hanging out with friends, and the beach.
             </p>
