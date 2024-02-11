d03822daaefdc1af77465d59cb9ef93fcdf5afa3
Tevin Campbell <75921907+Troyzhenny@users.noreply.github.com>
2024-02-10 23:41:28 -0500

    updated mailto -- footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -35,7 +35,7 @@ export default {
             <div id="sq2">
                 <h3>Contact</h3>
                 <span>
-                    <a v-for="(mailLink, index) in mailLinks" :key="index" :href="mailLink.url" target="_blank">
+                    <a v-for="(mailLink, index) in mailLinks" :key="index" :href="mailLink.url">
                         <i class="fa-solid fa-envelope"></i>
                         <p>{{ mailLink.name }}</p>
                     </a>
